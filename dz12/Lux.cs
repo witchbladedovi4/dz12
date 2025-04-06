b9f1358176f93f8ea12b8884f6d0cc93cfd5c0ab
witchbladedovi4 <pidzes@vk.com>
2025-04-07 04:34:24 +0500

    3

---
+++ dz12/Lux.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz12
+{
+    public class Lux :ITax, IConsolePrintable
+    {
+        public static string RuClassName { get; } = "Предмет росокши";
+
+        private string productName;
+        private double productCost;
+
+        public string ProductName
+        {
+            get => productName;
+            set => productName = value;
+        }
+
+        public double ProductCost
+        {
+            get => productCost;
+            set => productCost = value;
+        }
+
+        public double AddTax()
+        {
+            return productCost * 1.20;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Класс: {RuClassName}\n" +
+                $"Название товара: {ProductName}\n" +
+                $"Стоимость без налога: {ProductCost:F2}\n" +
+                $"Стоимость с налогом: {AddTax():F2}");
+        }
+    }
+}
--- dz12/Program.cs
+++ dz12/Program.cs
@@ -6,62 +6,76 @@ namespace dz12
     {
         static void Main(string[] args)
         {
-            var scooter = new Scooter
+            //var scooter = new Scooter
+            //{
+            //    RentalCostPerMinute = 1.2m,
+            //    Model = "Xiaomi",
+            //    Year = 2021
+            //};
+
+            //var scooterRental = new TransportRental<Scooter>("Иванов Иван Иванович", scooter, 120);
+            //Console.WriteLine(scooterRental.GetRentalInfo());
+            //Console.WriteLine($"Общая стоимость аренды: {scooterRental.CalculateTotalCost()}\n");
+
+
+
+
+            //var car = new Car
+            //{
+            //    RentalCostPerMinute = 15m,
+            //    LicensePlate = "777",
+            //    Make = "Toyota",
+            //    Model = "Camry",
+            //    Color = "X5"
+            //};
+
+            //var carRental = new TransportRental<Car>("Петров Петр Петрович", car, 60);
+            //Console.WriteLine(carRental.GetRentalInfo());
+            //Console.WriteLine($"Общая стоимость аренды: {carRental.CalculateTotalCost()}");
+
+            //Sword sword = new()
+            //{
+            //    WeaponPower = 10,
+            //    SpecialAbility = "Парирование",
+            //    Quality = SwordQuality.Уникальное
+            //};
+
+
+            //CharacterInGame<Sword> okr = new CharacterInGame<Sword>(
+            //    "Джон", "Орк", sword, 70);
+
+            //Console.WriteLine(okr.GetCharacterInfo());
+            //Console.WriteLine($"Суммарная сила: {okr.GetPower()}");
+
+
+            //Bow bow = new()
+            //{
+            //    WeaponPower = 20,
+            //    SpecialAbility = "Двойной выстрел",
+            //    Range = 100,
+            //    Accuracy = 75
+            //};
+
+
+            //CharacterInGame<Bow> archer = new CharacterInGame<Bow>(
+            //    "Эльза", "Эльф", bow, 7);
+
+            //Console.WriteLine(archer.GetCharacterInfo());
+            //Console.WriteLine($"Суммарная сила: {archer.GetPower()}");
+            var bread = new Necessites
             {
-                RentalCostPerMinute = 1.2m,
-                Model = "Xiaomi",
-                Year = 2021
+                ProductName = "Хлеб",
+                ProductCost = 50.2
             };
 
-            var scooterRental = new TransportRental<Scooter>("Иванов Иван Иванович", scooter, 120);
-            Console.WriteLine(scooterRental.GetRentalInfo());
-            Console.WriteLine($"Общая стоимость аренды: {scooterRental.CalculateTotalCost()}\n");
-
-
-
-
-            var car = new Car
+            var gold = new Lux
             {
-                RentalCostPerMinute = 15m,
-                LicensePlate = "777",
-                Make = "Toyota",
-                Model = "Camry",
-                Color = "X5"
+                ProductName = "Золото",
+                ProductCost = 113123
             };
 
-            var carRental = new TransportRental<Car>("Петров Петр Петрович", car, 60);
-            Console.WriteLine(carRental.GetRentalInfo());
-            Console.WriteLine($"Общая стоимость аренды: {carRental.CalculateTotalCost()}");
-
-            Sword sword = new()
-            {
-                WeaponPower = 10,
-                SpecialAbility = "Парирование",
-                Quality = SwordQuality.Уникальное
-            };
-
-
-            CharacterInGame<Sword> okr = new CharacterInGame<Sword>(
-                "Джон", "Орк", sword, 70);
-
-            Console.WriteLine(okr.GetCharacterInfo());
-            Console.WriteLine($"Суммарная сила: {okr.GetPower()}");
-
-
-            Bow bow = new()
-            {
-                WeaponPower = 20,
-                SpecialAbility = "Двойной выстрел",
-                Range = 100,
-                Accuracy = 75
-            };
-
-
-            CharacterInGame<Bow> archer = new CharacterInGame<Bow>(
-                "Эльза", "Эльф", bow, 7);
-
-            Console.WriteLine(archer.GetCharacterInfo());
-            Console.WriteLine($"Суммарная сила: {archer.GetPower()}");
+            bread.Print(); gold.Print();
         }
     }
 }
+
